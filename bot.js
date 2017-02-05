d3fa493f9d239f636fcb5da29d42c317d49b67a4
klh0058 <klh0058@auburn.edu>
2017-02-04 20:49:07 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = "fak";
+  botResponse = "/Fak";
   
     options = {
       hostname: 'api.groupme.com',
